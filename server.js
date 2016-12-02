28f79c8f038927f206af4ab2735127d116db1d2e
Eric <eric_hoog@hotmail.com>
2016-12-02 02:10:35 +0000

    removed routing issue

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var validurl = require('valid-url');
 
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
-var url = 'mongodb://' + process.env.IP + ':27017/erichoog-db';
+var url = 'mongodb://localhost:27017/erichoog-db';
 
 app.locals.pretty = true;
 app.set('port', (process.env.PORT || 8080));
@@ -23,9 +23,9 @@ app.get('/', function (req, res) {
   res.render('index', { title: 'URL Shortener microservice', mainHeader: 'API Basejump: URL Shortener microservice' });
 });
 
-app.get('/new', function (req, res) {
-  res.render('index', { title: 'URL Shortener microservice', mainHeader: 'API Basejump: URL Shortener microservice' });
-});
+// app.get('/new', function (req, res) {
+//   res.render('index', { title: 'URL Shortener microservice', mainHeader: 'API Basejump: URL Shortener microservice' });
+// });
 
 app.get('/new/:longurl(*)', function (req, res) {
   //get the long url and check if valid, else return error
@@ -35,30 +35,88 @@ app.get('/new/:longurl(*)', function (req, res) {
   }
   else {
     // if valid store in MongoDB and create a number for that url
-    // return original_url and short_url
-    res.json({success: "URL valid " + urlParam});
+    MongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+      res.json({error: "Unable to connect to the mongoDB server. Error:" + err})
+    } 
+    else {
+      console.log('Connection established to', url);
+      
+      var shorturls = db.collection( 'shorturls' );
+      shorturls.find({"original_url": urlParam})
+        .toArray( function( err, docs ) {
+          if ( err ) {
+            console.log( 'Error: Find operation failed' );
+            res.json({error: "Error: Find operation failed " + err})
+          } else {
+            if (docs.length == 1) {
+              console.log('Found existing shorturl.' + docs[0]);
+              // return original_url and short_url
+              var shortUrl = req.protocol + '://' + req.get('host') + '/' + docs[0].short_urlID;
+              res.json({original_url: urlParam, short_url: shortUrl});
+            }
+            else if (docs.length == 0) {
+                
+                var doc = {original_url: urlParam, short_urlID: 2};
+                  shorturls.insertOne(doc, 
+                    function (err, result){
+                      if (err) {
+                        console.log( 'Error: Inserting document' );
+                        res.json({error: "Error: Insert operation failed " + err})
+                      }
+                      else {
+                        var shortUrl = req.protocol + '://' + req.get('host') + '/' + doc.short_urlID;
+                        res.json({original_url: urlParam, short_url: shortUrl});
+                      }
+                    })
+                // }
+              // })
+            }
+            else {
+              res.json({error: "There was an error creating short url"});
+            }
+          }
+        });
+
+      //Close connection
+      db.close();
+    }
+  });
+    
+    
+    
+    
   }
 }); 
 
 app.get('/:id', function (req, res) {
   // get the mongo document that matches the id passed in.
   // redirect to that website
-  
-  // Use connect method to connect to the Server
   MongoClient.connect(url, function (err, db) {
     if (err) {
       console.log('Unable to connect to the mongoDB server. Error:', err);
       res.json({error: "Unable to connect to the mongoDB server. Error:" + err})
-    } else {
+    } 
+    else {
       console.log('Connection established to', url);
       
-      var shorturls = db.shorturls.find({"short_urlID": req.params.id.toString()});
-      // if (shorturls.count == 1) {
-      //   res.redirect(shorturls[0].original_url);
-      // }
-      // else {
-      //   res.json({error: "There was an error finding the original_url"});
-      // }
+      var shorturls = db.collection( 'shorturls' );
+      shorturls.find({"short_urlID": req.params.id.toString()})
+        .toArray( function( err, docs ) {
+          if ( err ) {
+            console.log( 'Error: Find operation failed' );
+          } else {
+            if (docs.length == 1) {
+              console.log('Found url.  Redirecting to: ' + docs[0].original_url );
+              res.redirect(docs[0].original_url);
+            }
+            else {
+              res.json({error: "There was an error finding the original_url"});
+            }
+          }
+        });
+
       //Close connection
       db.close();
     }
