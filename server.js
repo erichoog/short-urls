e7c72980af0f42325724d8707d94825d94236a33
Eric <eric_hoog@hotmail.com>
2017-04-09 20:45:21 +0000

    testing changes

--- server.js
+++ server.js
@@ -133,11 +133,10 @@ app.get('/:id', function (req, res) {
             }
             else if (docs.length == 0) {
               res.json({error: "There was an error finding the original_url.  Param: " + req.params.id.toString() + ' Doc Count: ' + docs.length});
-              console.log(res);
-              console.log(req.params.id.toString());
             }
             else { 
               res.json({error: "There was an error finding the original_url - too many"});
+              console.log(docs);
             }
           }
         });
